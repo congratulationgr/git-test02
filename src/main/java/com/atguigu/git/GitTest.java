bfef2b1f7aa16835c671de2f104b53ab408f0503
guoyabo <1878344634@qq.com>
2022-03-09 09:27:53 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("山水间歌声回荡");
+        System.out.println("心智所动就随缘去了");
     }
 }
