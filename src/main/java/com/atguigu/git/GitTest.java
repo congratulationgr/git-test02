d6706fbfa0ed0ef4b90efb22fa5b6e1b52450325
guoyabo <1878344634@qq.com>
2022-03-09 09:41:31 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,8 +2,10 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
+
         System.out.println("山水间歌声回荡");
         System.out.println("心智所动就随缘去了");
         System.out.println("姑娘你看你，一到家就哭泣");
+        System.out.println("hot-fix 为流言几局，真的有点傻气");
     }
 }
